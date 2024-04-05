d42142aac3cc77a7f9ba0b2df7f64d0d8da696b3
saf <asf>
2024-04-05 17:23:17 +0300

    Initial Commit

---
+++ SweaterShopDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweaterShopDemo
+//
+//  Created by Furkan Özdemir on 5.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Furkan Özdemir on 5.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
